5276b86684038afe9252e104b888c22c11c7bf28
Matteo Cremonesi <matteoc@fnal.gov>
2024-08-17 10:37:26 -0400

    Update btagcorrelation.py

--- analysis/processors/btagcorrelation.py
+++ analysis/processors/btagcorrelation.py
@@ -13,6 +13,7 @@ class BTagEfficiency(processor.ProcessorABC):
         self._year = year
         self._btagWPs = wp
         self._ids = ids
+        self._common = common
         match = self._common['match']
         self._ZHbbvsQCDwp = {
             '2016': 0.53,

d832f685f2734765afaca213af353b30ec2b0f63
Matteo Cremonesi <matteoc@fnal.gov>
2024-10-25 16:57:39 -0400

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
-export PYTHONPATH=~/.local/lib/python3.8/site-packages:$PYTHONPATH
-export PYTHONPATH=$PWD/analysis:$PYTHONPATH
-export PYTHONWARNINGS="ignore"
-#export PATH=~/.local/bin:$PATH
 cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 140:00 -pwstdin
 source /cvmfs/cms.cern.ch/rucio/setup-py3.sh
 export RUCIO_ACCOUNT=mcremone
+export PYTHONPATH=~/.local/lib/python3.8/site-packages#:$PYTHONPATH
+export PYTHONWARNINGS="ignore"
+#export PATH=~/.local/bin:$PATH
+
+

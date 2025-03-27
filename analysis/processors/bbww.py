5eb32f738daaa47dff0d085d8187ae9d4ee1691a
Sena Durgut <abrekk01@gmail.com>
2025-03-27 20:56:25 +0100

    cleaning up

--- analysis/processors/bbww.py
+++ analysis/processors/bbww.py
@@ -38,7 +38,6 @@ class AnalysisProcessor(processor.ProcessorABC):
         '2017': 41.48,
         '2018': 59.83
     }
-    #log_message(lumis)
 
     lumiMasks = {
         '2016postVFP': LumiMask(f"{path}/jsons/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"),
